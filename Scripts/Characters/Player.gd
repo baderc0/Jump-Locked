ccb07cac78c8322e0538c4a4f2a91adeff40bb27
baderc0 <andreibaderca1@gmail.com>
2021-01-09 11:22:47 -0500

    Got player movement working

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -2,11 +2,14 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const JUMP_VEL = -200
+const MAX_SPEED = 150
 
 var velocity = Vector2()
-var move_speed
+var move_speed = 100
 var gravity = 500
 var move_dir
+
+var idle_cutoff = MAX_SPEED / 6
 
 var is_grounded
 # Called when the node enters the scene tree for the first time.
@@ -14,17 +17,22 @@ func _ready():
 	pass 
 
 func apply_movement():
+	print(velocity)
+	is_grounded = is_grounded()
+	$GroundedLabel.text = str(is_grounded)
 	velocity = move_and_slide(velocity, UP)
 
 func apply_gravity(delta):
 	velocity.y += gravity * delta
 
 func handle_move_input():
-	velocity.x = lerp(velocity.x, move_speed * get_move_dir(), 0.2)
-	pass
+	velocity.x = lerp(velocity.x, MAX_SPEED * move_dir, 0.2)
 
-func get_move_dir():
-	return -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
+func update_move_dir():
+	move_dir =  -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
 
 func jump():
 	velocity.y = JUMP_VEL
+
+func is_grounded():
+	return true if $GroundedRaycast.is_colliding() else false
--- Scripts/Characters/PlayerFSM.gd
+++ Scripts/Characters/PlayerFSM.gd
@@ -1,5 +1,7 @@
 extends "res://Scripts/Other/StateMachine.gd"
 
+onready var state_label = get_parent().get_node("StateLabel")
+
 func _ready():
 	add_state("idle")
 	add_state("run")
@@ -9,30 +11,58 @@ func _ready():
 	call_deferred("set_state", states.idle)
 
 func _input(event):
-	pass
+	if event.is_action("jump") && parent.is_grounded():
+		parent.jump()
+		set_state(states.jump)
 
 # Calls funcs in Player.gd
 func _state_logic(delta):
-	parent.apply_gravity()
+	parent.update_move_dir()
 	parent.handle_move_input()
+	parent.apply_gravity(delta)
 	parent.apply_movement()
 
 func _get_transition(delta):
 	match state:
 		states.idle:
-			pass
+			if parent.is_grounded():
+				if abs(parent.velocity.x) > parent.idle_cutoff:
+					return states.run 
+			else:
+				if parent.velocity.y > 0:
+					return states.fall
+				else:
+					return states.jump
 		states.run:
-			pass
+			if parent.is_grounded():
+				if abs(parent.velocity.x) < parent.idle_cutoff:
+					return states.idle
+			else:
+				if parent.velocity.y > 0:
+					return states.fall
+				else:
+					return states.jump
 		states.jump:
-			pass
+			if parent.is_grounded():
+				if parent.velocity.x < parent.idle_cutoff:
+					return states.idle
+				else:
+					return states.run
+			elif parent.velocity.y >= 0:
+				return states.fall
 		states.fall:
-			pass
+			if parent.is_grounded():
+				if abs(parent.velocity.x) < parent.idle_cutoff:
+					return states.idle
+				else:
+					return states.run
 		states.attack:
 			pass
 	return null
 
 # setting anim, tween, timers, etc
 func _enter_state(new_state, old_state):
+	state_label.text = states.keys()[state]
 	pass
 
 func _exit_state(old_state, new_state):
